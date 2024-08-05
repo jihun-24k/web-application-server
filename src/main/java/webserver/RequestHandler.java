dc295e6ec306531784afc96608272f6311b30b1c
jihun-24k <gi020477@gmail.com>
2024-08-05 17:16:27 +0900

    feat: GET 방식으로 회원가입

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -12,8 +12,11 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 import java.nio.file.Files;
+import java.util.Map;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -34,8 +37,21 @@ public class RequestHandler extends Thread {
 
             String line = bufferIn.readLine();
             String[] tokens = line.split(" ");
+
             String httpMethod = tokens[0];
             String url = tokens[1];
+
+            int index = url.indexOf("?");
+            String requestUrl = url.substring(0, index);
+            String params = url.substring(index + 1);
+            Map<String, String> userInfo = HttpRequestUtils.parseQueryString(params);
+
+            User createdUser = new User(
+                        userInfo.get("userId"),
+                        userInfo.get("password"),
+                        userInfo.get("name"),
+                        userInfo.get("email")
+                    );
 
             while (!"".equals(line) && line != null) {
                 log.debug("HTTP Header Info = {}", line);
@@ -43,7 +59,7 @@ public class RequestHandler extends Thread {
             }
 
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            byte[] body = Files.readAllBytes(new File("./webapp" + requestUrl).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {

8442e5a42beeb862f07087e10113e9cf593bc12c
jihun-24k <gi020477@gmail.com>
2024-08-06 08:59:07 +0900

    feat: POST 방식으로 회원가입

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -12,11 +12,13 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -40,22 +42,49 @@ public class RequestHandler extends Thread {
 
             String httpMethod = tokens[0];
             String url = tokens[1];
+            User createdUser;
 
             int index = url.indexOf("?");
-            String requestUrl = url.substring(0, index);
-            String params = url.substring(index + 1);
-            Map<String, String> userInfo = HttpRequestUtils.parseQueryString(params);
+            String requestUrl = url;
+            if (index > 0) {
+                requestUrl = url.substring(0, index);
+                String params = url.substring(index + 1);
+                Map<String, String> userInfo = HttpRequestUtils.parseQueryString(params);
 
-            User createdUser = new User(
+                createdUser = new User(
                         userInfo.get("userId"),
                         userInfo.get("password"),
                         userInfo.get("name"),
                         userInfo.get("email")
-                    );
+                );
+            }
+
+            Map<String, String> httpHeader = new HashMap<>();
 
             while (!"".equals(line) && line != null) {
                 log.debug("HTTP Header Info = {}", line);
                 line = bufferIn.readLine();
+
+                if (line == null || "".equals(line)) {
+                    break;
+                }
+
+                String[] info = line.split(": ");
+                httpHeader.put(info[0], info[1]);
+            }
+
+            if (httpMethod.equals("POST")) {
+                int contentLength = Integer.parseInt(httpHeader.get("Content-Length"));
+                String httpBody = IOUtils.readData(bufferIn, contentLength);
+
+                Map<String, String> userInfo = HttpRequestUtils.parseQueryString(httpBody);
+
+                createdUser = new User(
+                        userInfo.get("userId"),
+                        userInfo.get("password"),
+                        userInfo.get("name"),
+                        userInfo.get("email")
+                );
             }
 
             DataOutputStream dos = new DataOutputStream(out);

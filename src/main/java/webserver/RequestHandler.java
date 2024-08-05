5a5ce70fd2b1eb0ba778b74150c3b7b38ee8a499
jihun-24k <gi020477@gmail.com>
2024-08-05 16:53:37 +0900

    feat: index.html 응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,17 @@
 package webserver;
 
+import static java.lang.System.in;
+
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,9 +29,21 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            BufferedReader bufferIn = new BufferedReader(new InputStreamReader(in));
+
+            String line = bufferIn.readLine();
+            String[] tokens = line.split(" ");
+            String httpMethod = tokens[0];
+            String url = tokens[1];
+
+            while (!"".equals(line) && line != null) {
+                log.debug("HTTP Header Info = {}", line);
+                line = bufferIn.readLine();
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
